9151ef6a12831bddfbf0f4723c264396a9132f9b
BrunoViola <brunohenriquesilvaviola@gmail.com>
2024-08-18 23:01:06 -0300

    Reconhece a cadeia 'print num=num'

--- analisador-sintatico-01/LISTA_9_EXERCICIO_1.c
+++ analisador-sintatico-01/LISTA_9_EXERCICIO_1.c
@@ -2,6 +2,90 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum {
+    IF = 1,
+    THEN,
+    ELSE,
+    BEGIN,
+    END,
+    PRINT,
+    SEMI,
+    NUM,
+    EQ
+} TokenType;
+
+// Função para converter o valor do enum em uma string
+const char* tokenTypeToString(TokenType token) {
+    switch (token) {
+        case IF: return "if";
+        case THEN: return "then";
+        case ELSE: return "else";
+        case BEGIN: return "begin";
+        case END: return "end";
+        case PRINT: return "print";
+        case SEMI: return ";";
+        case NUM: return "num";
+        case EQ: return "=";
+        default: return "UNKNOWN";
+    }
+}
+
+int token_posicao = 0;
+int erro_sintatico = 0;
+
+void S(int *vetor);
+void L(int *vetor);
+void E(int *vetor);
+
+void eat(int t, int *vetor){
+   if(vetor[token_posicao]==t){
+      token_posicao++;
+   }else{
+      printf("\nErro");
+      //error();
+   }
+}
+
+void S(int *vetor){
+   switch (vetor[token_posicao])
+   {
+   case IF:
+      eat(IF, vetor); E(vetor); eat(THEN, vetor); S(vetor); eat(ELSE, vetor); S(vetor); break;
+   case BEGIN: eat(BEGIN, vetor); S(vetor); L(vetor); break;
+   case PRINT: eat(PRINT, vetor); E(vetor); break;
+   
+   default:
+      printf("Erro no S");
+      break;
+   }
+}
+
+void L(int *vetor){
+   switch (vetor[token_posicao])
+   {
+   case END:
+      eat(END, vetor); break;
+   case SEMI: eat(SEMI, vetor); S(vetor); L(vetor); break;
+  
+   default:
+      printf("Erro no L");
+      break;
+   }
+}
+
+void E(int *vetor){
+   switch (vetor[token_posicao])
+   {
+   case NUM:
+      eat(NUM, vetor); break;
+  
+   default:
+      printf("Erro sintatico em: %s ESPERADO: num", tokenTypeToString(vetor[token_posicao]));
+      erro_sintatico = 1;
+      break;
+   }
+}
+
 int main(){
    char *string = NULL;
    char c;
@@ -33,30 +117,37 @@ int main(){
       if(aux[i]=='i' && aux[i+1] == 'f'){ //reconhece o IF
          printf("IF\n");
          vetor[posicao] = 1;
+         posicao++;
          i+=1;
       }else if(aux[i]=='t' && aux[i+1]=='h' && aux[i+2]=='e' && aux[i+3]=='n'){ //reconhece THEN
          printf("THEN\n");
          vetor[posicao] = 2;
+         posicao++;
          i+=3;
       }else if(aux[i]=='e' && aux[i+1]=='l' && aux[i+2]=='s' && aux[i+3]=='e'){ //reconhece ELSE
          printf("ELSE\n");
          vetor[posicao] = 3;
+         posicao++;
          i+=3;
       }else if(aux[i]=='b' && aux[i+1]=='e' && aux[i+2]=='g' && aux[i+3]=='i' && aux[i+4]=='n'){ //reconhece BEGIN
          printf("BEGIN\n");
          vetor[posicao] = 4;
+         posicao++;
          i+=4;
       }else if(aux[i]=='e' && aux[i+1]=='n' && aux[i+2]=='d'){ //reconhece END
          printf("END\n");
          vetor[posicao] = 5;
+         posicao++;
          i+=2;
       }else if(aux[i]=='p' && aux[i+1]=='r' && aux[i+2]=='i' && aux[i+3]=='n' && aux[i+4]=='t'){ //reconhece PRINT
          printf("PRINT\n");
          vetor[posicao] = 6;
+         posicao++;
          i+=4;
       }else if(aux[i]==';'){ //reconhece SEMI
          printf("SEMI\n");
          vetor[posicao] = 7;
+         posicao++;
       }else if((aux[i]=='+' || aux[i]=='-' || (aux[i]>=48 && aux[i]<=57))){ //reconhece NUM
          if(aux[i] == '+' || aux[i] == '-') {
             i++; // avança para o proximo caractere
@@ -69,18 +160,24 @@ int main(){
          if(eh_num){ 
             printf("NUM\n");
             vetor[posicao] = 8;
+            posicao++;
          }else{
             printf("ERRO\n");
          }
       }else if(aux[i]=='='){ //reconhece EQ
          printf("EQ\n");
          vetor[posicao] = 9;
+         posicao++;
       }
       i++;
    }
+
+   for(int i=0; i<=posicao;i++) printf("%d", vetor[i]);
+   printf("\n");
    
    // Libera a memória alocada para a string
    free(string);
-
+   S(vetor);
+   if(!erro_sintatico) printf("CADEIA ACEITA");
    return 0;
 }
